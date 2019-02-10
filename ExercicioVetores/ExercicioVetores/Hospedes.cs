e35c3fd6fc34a60472c037f24749620cbdb0723c
Ramon Sobreira <47504998+raamon147@users.noreply.github.com>
2019-02-10 16:51:21 -0200

    Add files via upload

---
+++ ExercicioVetores/ExercicioVetores/Hospedes.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ExercicioVetores
+{
+    class Hospedes
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public int Quarto { get; private set; }
+
+        public Hospedes (string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+        public override string ToString()
+        {
+            return Quarto + ": " + Nome + ", " + Email;
+        }
+    }
+}
---
+++ ExercicioVetores/ExercicioVetores/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ExercicioVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos quartos serao alugados: ");
+            int n = int.Parse(Console.ReadLine());
+            Hospedes[] hospedes = new Hospedes[11];
+
+            while (n> hospedes.Length)
+            {
+                Console.WriteLine("O Hotel só possui " + (hospedes.Length)  + " Quartos, Escolha apenas esses !");
+                n = int.Parse(Console.ReadLine());
+            }
+            
+            for (int i = 1; i <= n; i++)
+            {
+                
+                Console.WriteLine("Hospede #" + i + ":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto : ");
+                int quarto = int.Parse(Console.ReadLine());
+
+
+                while (quarto > hospedes.Length || quarto ==0)
+                {
+                    Console.WriteLine("Quarto inexistente, digite um quarto entre 1 e "
+                        + hospedes.Length);
+                    quarto = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine();
+
+
+                while (hospedes[quarto] != null)
+                {
+                    Console.WriteLine("quarto ocupado, digite outro");
+                    quarto = int.Parse(Console.ReadLine());
+                    while (quarto > hospedes.Length)
+                    {
+                        Console.WriteLine("Quarto inexistente, digite um quarto entre 1 e "
+                        + hospedes.Length);
+                        quarto = int.Parse(Console.ReadLine());
+                    }
+                }
+                hospedes[quarto] = new Hospedes(nome, email, quarto);
+                }
+
+            Console.WriteLine("Quartos ocupados");
+            for (int i = 0; i <= 10; i++)
+            {
+                if(hospedes[i] != null)
+                {
+                    
+                    Console.WriteLine(hospedes[i].ToString());
+                }
+            }
+        }
+    }
+}
